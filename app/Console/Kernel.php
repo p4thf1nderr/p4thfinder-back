97bd999cd2c340b739b9ed228aaf5c9eb8458cbd
a_tretyakov <artem.tretyakov.91@inbox.ru>
2018-06-17 00:20:45 +0300

    add command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        \App\Console\Commands\Communal::class
     ];
 
     /**
@@ -24,6 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        //
+        $schedule->command('com')
+                ->everyMinute();
     }
 }
