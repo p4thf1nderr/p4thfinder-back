b806fc337a807df6556838d498dd8338f29b6db2
a_tretyakov <artem.tretyakov.91@inbox.ru>
2018-06-20 06:45:07 +0300

    fix

--- app/Http/Controllers/CommunalData/CommunalController.php
+++ app/Http/Controllers/CommunalData/CommunalController.php
@@ -1,19 +1,15 @@
 <?php
 namespace App\Http\Controllers\CommunalData;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 
 class CommunalController extends Controller
 {
     public static function index($value='')
     {
-		$token = env('BOT_TOKEN');
-		
-		Log::warning('ddd');
-
+    	/* $token = env('BOT_TOKEN');
 		$bot = new \TelegramBot\Api\Client($token);
 		// команда для start
 		$bot->command('start', function ($message) use ($bot) {
@@ -26,37 +22,15 @@ class CommunalController extends Controller
 		/help - вывод справки';
 		    $bot->sendMessage($message->getChat()->getId(), $answer);
 		});
-		$bot->run();
+		$bot->run(); */
 
-		/* $token = env('BOT_TOKEN');
+		$token = env('BOT_TOKEN');
 		$chatId = env('CHAT_ID');
 		$messageText = 'Передайте показания';
 
 		$bot = new \TelegramBot\Api\BotApi($token);
 
 		$bot->sendMessage($chatId, $messageText);
-		$bot->run(); */
-
-		
-
-		// команда push получает текст вида газ#24,хол#10,гор#40
-
-		// после этого раскладываем какая информация к какой службе относится
-		// когда данные разбиты на переменные, отправляем их на почтовый сервис, где происходит подстановка и отправка
-
-		/* $bot->command('push', function ($message) use ($bot, $chatId) {
-			$text = $message->getText();
-			$param = str_replace('/push ', '', $text);
-			$answer = 'Неизвестная команда';
-			if (!empty($param))
-			{
-				$answer = 'Привет, ' . $param;
-			}
-			$bot->sendMessage($chatId, $answer);
-		});
-
-		$bot->run(); */
-
-
+		$bot->run();
     }
 }
