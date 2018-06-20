749e41d100ed67678d8a7c1e468e31ac2d48700d
a_tretyakov <artem.tretyakov.91@inbox.ru>
2018-06-20 06:12:16 +0300

    fix

--- app/Http/Controllers/CommunalData/CommunalController.php
+++ app/Http/Controllers/CommunalData/CommunalController.php
@@ -9,7 +9,7 @@ class CommunalController extends Controller
 {
     public static function index($value='')
     {
-    	/* $token = env('BOT_TOKEN');
+    	$token = env('BOT_TOKEN');
 		$bot = new \TelegramBot\Api\Client($token);
 		// команда для start
 		$bot->command('start', function ($message) use ($bot) {
@@ -22,16 +22,16 @@ class CommunalController extends Controller
 		/help - вывод справки';
 		    $bot->sendMessage($message->getChat()->getId(), $answer);
 		});
-		$bot->run(); */
+		$bot->run();
 
-		$token = env('BOT_TOKEN');
+		/* $token = env('BOT_TOKEN');
 		$chatId = env('CHAT_ID');
 		$messageText = 'Передайте показания';
 
 		$bot = new \TelegramBot\Api\BotApi($token);
 
 		$bot->sendMessage($chatId, $messageText);
-		$bot->run();
+		$bot->run(); */
 
 		
 
