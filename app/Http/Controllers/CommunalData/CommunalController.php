c783b0e5c457f21bbdd41d18156a67bc92964f1e
a_tretyakov <artem.tretyakov.91@inbox.ru>
2018-06-21 22:42:22 +0300

    fix

--- app/Http/Controllers/CommunalData/CommunalController.php
+++ app/Http/Controllers/CommunalData/CommunalController.php
@@ -23,6 +23,20 @@ class CommunalController extends Controller
 		/help - вывод справки';
 		    $bot->sendMessage($message->getChat()->getId(), $answer);
 		});
+
+
+		$bot->command('hello', function ($message) use ($bot) {
+		    $text = $message->getText();
+		    $param = str_replace('/hello ', '', $text);
+		    $answer = 'Неизвестная команда';
+		    if (!empty($param))
+		    {
+		    	$answer = 'Привет, ' . $param;
+		    }
+		    $bot->sendMessage($message->getChat()->getId(), $answer);
+		});
+
+
 		$bot->run();
 
 		//Log::warning('бот запускается');
