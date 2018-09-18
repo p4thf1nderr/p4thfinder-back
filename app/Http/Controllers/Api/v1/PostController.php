0cacb739e751c058321eb5edd54a449a89a09ca8
a_tretyakov <artem.tretyakov.91@inbox.ru>
2018-09-18 13:59:25 +0300

    fix

--- app/Http/Controllers/Api/v1/PostController.php
+++ app/Http/Controllers/Api/v1/PostController.php
@@ -69,6 +69,8 @@ class PostController extends Controller
             $post->text  = $args['input']['text'];
         }
 
+        $post->tags()->sync($args['input']['checked']);
+
         $post->save();
 
         return $post;
